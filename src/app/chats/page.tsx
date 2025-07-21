f2edbdf0c36458d5592d052c1380c313df4375e2
1faadi <fahadnshuu1@gmail.com>
2025-07-21 13:00:01 +0500

    hey

--- src/app/chats/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function EmptyChatPage() {
-    return (
-      <div className="p-8 text-center text-muted-foreground">
-        <p>Select a chat to continue or start a new one</p>
-      </div>
-    );
-  }
-  
